015bf535cee3af40741b96e33b737fc4de3f4163
Brandon Leeds <brandonleeds@gmail.com>
2018-01-19 23:44:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,10 @@
 class Dog
 
   attr_accessor :name, :breed
+  attr_reader :id
 
-  def initialize(name, breed)
+  def initialize(id=nil, name, breed)
+    @id = id
     @name = name
     @breed = breed
   end
